52bdb327c56088f54d05b6b7cc3767a02d165c1f
Shubhra Singhal <shubhrasinghal2015@gmail.com>
2021-01-20 23:25:47 +0530

    check visible products on basis of vegetable to add in cart code is added.

--- cypress/integration/myexamples/Test1.js
+++ cypress/integration/myexamples/Test1.js
@@ -8,8 +8,15 @@ describe('My First Test', function() {
       //visit is used to visit any URL
      cy.visit("https://rahulshettyacademy.com/seleniumPractise/#/");
      cy.get('.search-keyword').type("ca");
+     cy.wait(2000)
      //get acts like findelement of selenium use the chai assertion should
-     cy.get('.product').should('have.length',4);
+     //visible is jquery function which will help to view only visible items
+     cy.get('.product:visible').should('have.length',4);
+     
+     //Parent child chaining, find will look only inside the particular given scope not whole page
+     cy.get(".products").find(".product").should('have.length',4);
 
+     //click on add to cart for 2nd product
+     cy.get('.products').find(".product").eq(2).contains('ADD TO CART').click()
     })
   })
