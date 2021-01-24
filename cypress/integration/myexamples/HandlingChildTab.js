4391cec4a6477cddf9f5ca4e0739e5d30ae2917d
Shubhra Singhal <shubhrasinghal2015@gmail.com>
2021-01-25 01:53:11 +0530

    Handling child tab using jQuery removeAttr

---
+++ cypress/integration/myexamples/HandlingChildTab.js
@@ -0,0 +1,21 @@
+// <reference types="Cypress" />
+
+describe('My fourth test case', function() {
+    it('Complete Tests ', function() {
+      //test steps for that test will go inside it
+      //visit is used to visit any URL
+     cy.visit("https://rahulshettyacademy.com/AutomationPractice/");
+
+     //Calling the 'removeAttr' function of jQuery and passing the value will remove that CSS part 
+     cy.get("#opentab").invoke('removeAttr','target').click();
+
+     //Retrieve Url
+     cy.url().should('include','index');
+
+     //To go back to previous page
+     cy.go('back');
+
+     cy.url().should('include','AutomationPractice');
+
+    })
+})
