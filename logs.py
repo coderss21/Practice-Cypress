521d7beb4230d9916b30fe6bceb6a308e8848d5a
Shubhra Singhal <45839322+coderss21@users.noreply.github.com>
2021-02-26 12:36:02 +0530

    Updated log.py with sleep

--- logs.py
+++ logs.py
@@ -1,5 +1,12 @@
+import time
 print("Hello World")
 i = 1
-while i <= 10000:
+while i <= 100:
     print("Welcome ", i,"times")
     i += 1
+
+i=10
+while i<=1000 :
+    print("Hello I am ",i,"times")
+    time.sleep(3)
+    i+=100
